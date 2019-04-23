3093f9a8daf70b34351d47c84bcf00571ec6255e
Justin Hall <hall.justin.d@gmail.com>
2019-04-23 23:34:19 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,6 +1,6 @@
 class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
-    create_table :candies do |t|
+    create_table :costumes do |t|
       t.string :name
       t.integer :calories
       t.timestamps
